99f66caec174a76185678375ae6c027deb0e1f02
bella-cockrell <arabella.cockrell@gmail.com>
2020-09-18 10:38:03 +0100

    Commenting out nil test, created testing hash, tested and passed new test.

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,5 +1,5 @@
 class Bowling
-    def foo_bar
-        "string"
+    def foo_bar(input_string)
+        input_string
     end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -7,8 +7,20 @@ RSpec.describe Bowling do
       expect(bowling).to be_kind_of(Bowling)
     end
 
-    it 'returns nil if nothing is given to it' do
-      expect(Bowling.new.foo_bar).to eq("string")
+    # it 'returns nil if nothing is given to it' do
+    #   expect(Bowling.new.foo_bar).to eq("string")
+    # end
+    
+    testing_hash = {
+      "oryeh" => "oryeh",
+      "foobar" => "foobar",
+      "Kombucha" => "Kombucha"
+    }
+
+    testing_hash.each do |string, output|
+      it "returns the same string back" do
+        expect(Bowling.new.foo_bar(string)).to eq(output)
+      end
     end
     
 end
